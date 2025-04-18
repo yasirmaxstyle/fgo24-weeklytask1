fd51d9de538c8d242520aa63fbda7211bc039ba3
yasirmaxstyle <yasirmu77@gmail.com>
2025-04-18 11:20:52 +0700

    add exercise of using object

---
+++ 2_javascript1.js
@@ -0,0 +1,13 @@
+const product = {
+    name: "kemeja pria lengan panjang slimfit kemeja kerja casual tbb39 - Hitam, M",
+    rating: 4.5,
+    price: 135000,
+    isActive: true,
+    images: ["black.png", "navy.png", "maroon.png", "white.png", "coksu.png", "grey.png"],
+    description: "kemeja adem mantep",
+    variants: {
+        warna: "hitam",
+        options: ['M', 'L', 'XL', 'XXL']
+    },
+    storeName: "Toko Baju"
+}
