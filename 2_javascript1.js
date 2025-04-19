6bda0a8a88b505d8a0a32fc56f9721d7c5af215d
yasirmaxstyle <yasirmu77@gmail.com>
2025-04-19 13:35:18 +0700

    add second key name

--- 2_javascript1.js
+++ 2_javascript1.js
@@ -1,12 +1,13 @@
 const product = {
-    name: "kemeja pria lengan panjang slimfit kemeja kerja casual tbb39 - Hitam, M",
+    name: "kemeja pria lengan panjang slimfit kemeja kerja casual tbb39 - ",
     rating: 4.5,
     price: 135000,
     isActive: true,
     images: ["black.png", "navy.png", "maroon.png", "white.png", "coksu.png", "grey.png"],
     description: "kemeja adem mantep",
+    name: "kemeja pria lengan panjang slimfit kemeja kerja casual tbb39 - Hitam, M",
     variants: {
-        warna: "hitam",
+        warna: "Hitam",
         options: ['M', 'L', 'XL', 'XXL']
     },
     storeName: "Toko Baju"
